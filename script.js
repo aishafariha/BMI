b8745a1ca37c4732a811982c65dfeb537271f8d4
aishafariha <122160899+aishafariha@users.noreply.github.com>
2025-08-10 01:55:12 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,67 +1,52 @@
-const username=document.querySelector("#username");
-const dob=document.querySelector("#dob");
-const btn=document.querySelector(".btn");
-const showtime=document.querySelector(".u-time");
-const month=[31,28,31,30,31,30,31,31,30,31,30,31];
-btn.addEventListener("click",calculateAge);//here we add buttoon and if e click button then it call the calculateAge function
-function calculateAge(e) {
-    e.preventDefault()
-    let today = new Date();
-    let dobInput = new Date(dob.value);
-    let birthMonth,birthDate,birthyear;
-    let birthDetails=
-    {
-date:dobInput.getDate(),
-month:dobInput.getMonth() + 1,
-year:dobInput.getFullYear(),
+// BMI - kg/m2
+// Normal - 18.5 -24.9
+// Underweight - < 18.5
+// Overweight -  25 - 29.9
+// Obese -  30 and above
 
-    };
-    let currentYear= today.getFullYear();
-    let currentMonth=today.getMonth() + 1;
-    let currentDate= today.getDate();
-    if(
-        birthDetails.year>currentYear ||
-        (birthDetails.month>currentMonth && birthDetails.year==currentYear)||
-         (birthDetails.date>currentDate && birthDetails.month==currentMonth && birthDetails.year==currentYear)
-    )
-    {
-        alert("Invalid date");
-        return;
-    }
-   
-    birthyear= currentYear-birthDetails.year;//here if we subtract currentyear from birthyear than we find our peresnt birth year
-    //giving condition to find my birthmonth
-    if(currentMonth>=birthDetails.month){
-        birthMonth=currentMonth-birthDetails.month;
-    }
-    else
-    {
-        birthYear--; //here i decrease 1 year means it decrease 1 value
-        birthMonth= 12 + currentMonth-birthDetails.month;// here e decrease the birthyear thats why i add 12 because 1year =12 month 
-    }
-    if(currentDate>=birthDetails.date)
-    {
-        birthDate=currentDate-birthDetails.date;
-    }
-    else
-    {
-        birthMonth--;//here i decrease 1 month means it decrease 1 value
-        let days = months[currentMonth -2];// the all month days are not same lie 31,30,28 thats reason we suntract currecmt month from 2 and i mae days function
-        birthDate= days + currentDate-birthDetails.date;
-    }
-    if (birthMonth<0)
-    {
-birthMonth=11;// if the birthdate given into negative that means it is the last year birthday and it is december
-//  but i give 11 because index is count from 0
-birthYear--;
-    }
-    displayResult(birthDate,birthMonth,birthYear);
+const btn = document.querySelector(".btn"),
+  result = document.querySelector(".result"),
+  reset = document.querySelector(".reset");
 
-function displayResult(bDate,bMonth,bYear)
-{
-    showName.textContent= username.value;
-    document.querySelector("age-year").textContent =bYear + "Years";
-    document.querySelector("age-month").textContent =bMonth + "Months";
-    document.querySelector("age-day").textContent =bDate + "Day";
+btn.addEventListener("click", calculateBMI);
+
+function calculateBMI(e) {
+  e.preventDefault();
+  let height = document.querySelector(".height").value;
+  let weight = document.querySelector(".weight").value;
+
+  console.log(height);
+
+  // Validate Input
+  if (height === "" || isNaN(height)) {
+    return (result.innerHTML = "Provide a valid Height!");
+  } else if (weight === "" || isNaN(weight)) {
+    return (result.innerHTML = "Provide a valid Height!");
+  } else {
+    height = height / 100;
+    let bmi = (weight / Math.pow(height, 2)).toFixed(2);
+    console.log(bmi);
+    //   Categorize result
+    if (bmi < 18.5) {
+      showResult(`Underweight: <span>${bmi}</span>`, "orange");
+    } else if (bmi >= 18.5 && bmi < 24.9) {
+      showResult(`Normal: <span>${bmi}</span>`, "green");
+    } else if (bmi >= 25.0 && bmi < 29.9) {
+      showResult(`Overweight: <span>${bmi}</span>`, "blue");
+    } else {
+      showResult(`Obese: <span>${bmi}</span>`, "red");
+    }
+  }
+  reset.style.display = "block";
 }
+
+function showResult(val, color) {
+  result.style.backgroundColor = color;
+  return (result.innerHTML = val);
 }
+
+reset.addEventListener("click", () => {
+  document.querySelector("form").reset();
+  reset.style.display = "none";
+  result.style.display = "none";
+});
